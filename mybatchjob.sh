b5ed78904ff78cc1b4afec8d1829f799741fc0c7
Alex Schmid <alexandriaschmid3@gmail.com>
2022-01-03 15:14:11 -0500

    Fixing the output paths

--- mybatchjob.sh
+++ mybatchjob.sh
@@ -3,8 +3,8 @@
 #SBATCH --mem-per-cpu=2G
 #SBATCH --partition=sched_mit_sloan_batch
 #SBATCH --time=0-00:10
-#SBATCH -o /home/aschmid/myoutputfile.out
-#SBATCH -e /home/aschmid/myerrorfile.err
+#SBATCH -o /home/aschmid/clusterrepo/myoutputfile.out
+#SBATCH -e /home/aschmid/clusterrepo/myerrorfile.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=aschmid@mit.edu
 
