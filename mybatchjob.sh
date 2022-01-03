ae8d9e8da29dc751058ae19412a3978bc8ddee38
Alex Schmid <alexandriaschmid3@gmail.com>
2022-01-03 15:17:39 -0500

    Removing my username from the batch file

--- mybatchjob.sh
+++ mybatchjob.sh
@@ -3,10 +3,10 @@
 #SBATCH --mem-per-cpu=2G
 #SBATCH --partition=sched_mit_sloan_batch
 #SBATCH --time=0-00:10
-#SBATCH -o /home/aschmid/clusterrepo/myoutputfile.out
-#SBATCH -e /home/aschmid/clusterrepo/myerrorfile.err
+#SBATCH -o /home/username/clusterrepo/myoutputfile.out
+#SBATCH -e /home/username/clusterrepo/myerrorfile.err
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=aschmid@mit.edu
+#SBATCH --mail-user=username@mit.edu
 
 module load julia/1.5.2
 
