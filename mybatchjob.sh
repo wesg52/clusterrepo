2dccca4ef8e0b640734019f2a9365721cfbd3556
Big Brother <rwg97@cornell.edu>
2022-01-04 18:06:00 -0500

    update sbatch username

--- mybatchjob.sh
+++ mybatchjob.sh
@@ -3,10 +3,10 @@
 #SBATCH --mem-per-cpu=2G
 #SBATCH --partition=sched_mit_sloan_batch
 #SBATCH --time=0-00:10
-#SBATCH -o /home/username/clusterrepo/myoutputfile.out
-#SBATCH -e /home/username/clusterrepo/myerrorfile.err
+#SBATCH -o /home/wesg/clusterrepo/myoutputfile.out
+#SBATCH -e /home/wesg/clusterrepo/myerrorfile.err
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=username@mit.edu
+#SBATCH --mail-user=wesg@mit.edu
 
 module load julia/1.5.2
 
